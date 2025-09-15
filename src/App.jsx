9afc4808d472181fb7d3a61dfd726d750ef5611e
ColdDdev <colindecorce@gmail.com>
2025-09-15 13:02:42 +0100

    DD

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import {RouterProvider, createHashRouter} from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
 import Verify from "./pages/auth/Verify";
@@ -25,57 +25,55 @@ import Investmentpay from "./pages/dashboard/Investmentpay";
 import Withdrawals from "./pages/dashboard/Withdrawals";
 
 const App = () => {
-  const router = createHashRouter([
-    {
-      path: "/",
-      element: <Login />,
-    },
-    {
-      path: "register",
-      element: <Signup />,
-    },
-    {
-      path: "forgotten-password",
-      element: <Forgottenpassword />,
-    },
-    {
-      path: "reset-password/:id/:token",
-      element: <Reset />,
-    },
-    {
-      path: "verify",
-      element: <Verify />,
-    },
-    {
-      path: "/dashboard",
-      errorElement: <ErrorPage />,
-      element: <DashboardRoute element={<Dashboard />}></DashboardRoute>,
-      children: [
-        { path: "", element: <DashboardHome /> },
-        { path: "deposit", element: <Deposit /> },
-        { path: "deposit-pay", element: <DepositPay /> },
-        { path: "my-plans", element: <MyPlans /> },
-        { path: "new-withdrawal", element: <NewWithdrawal /> },
-        { path: "new-bonus", element: <NewBonus /> },
-        { path: "my-invest", element: <Investments /> },
-        { path: "my-invests/:id", element: <InvestmentsView /> },
-        { path: "my-deposit/:id", element: <DepositHistoryView /> },
-        { path: "withdraw-details/:id", element: <Withdrawals /> },
-        { path: "my-deposit", element: <DepositHistory /> },
-        { path: "my-withdrawal", element: <MyWithdrawal /> },
-        { path: "profile", element: <Profile /> },
-        { path: "referus", element: <Referral /> },
-        { path: "profit", element: <Profit /> },
-        { path: "Investmentpay", element: <Investmentpay /> },
-      ],
-    },
-  ]);
+    const router = createHashRouter([
+        {
+            path: "",
+            element: <Login />,
+        },
+        {
+            path: "register",
+            element: <Signup />,
+        },
+        {
+            path: "forgotten-password", element: <Forgottenpassword />,
+        },
+        {
+            path: "reset-password/:id/:token", element: <Reset/>,
+        },
+        {
+            path: "verify",
+            element: <Verify />,
+        },
+        {
+            path: "/dashboard",
+            errorElement: <ErrorPage />,
+            element: <DashboardRoute element={<Dashboard />}></DashboardRoute>,
+            children: [
+                { path: "", element: <DashboardHome />, },
+                { path: "deposit", element: <Deposit />, },
+                { path: "deposit-pay",element: <DepositPay />, },
+                { path: "my-plans", element: <MyPlans />,},
+                { path: "new-withdrawal",element: <NewWithdrawal />,},
+                { path: "new-bonus",element: <NewBonus />,},
+                { path: "my-invest",element: <Investments />,},
+                { path: "my-invests/:id",element: <InvestmentsView />,},
+                { path: "my-deposit/:id",element: <DepositHistoryView/>,},
+                { path: "withdraw-details/:id",element: <Withdrawals/>,},
+                { path: "my-deposit",element: <DepositHistory />,},
+                { path: "my-withdrawal",element: <MyWithdrawal />,},
+                { path: "profile",element: <Profile />,},
+                { path: "referus",element: <Referral />,},
+                { path: "profit",element: <Profit />,},
+                { path: "Investmentpay",element: <Investmentpay />,},
+            ],
+        },
+    ]);
 
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 };
 
 export default App;
